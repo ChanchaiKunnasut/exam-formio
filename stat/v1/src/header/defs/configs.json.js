4d3f07c185d66c30f7d8a542dc94c36b91285771
Alfred Madl <a.madl@togetherteam.co.th>
2018-02-11 05:50:57 +0000

    Updated configs.json.js

--- stat/v1/src/header/defs/configs.json.js
+++ stat/v1/src/header/defs/configs.json.js
@@ -1,7 +1,7 @@
-  var confObjs={}
-  "forms":[{"path":"./defs","formgroup":"","formname":"form", "formversion":""}],
-  "apps":[{"path":"./defs","name":"app"}],
-  "brands":[{"path":"./defs","name":"brand"}],
-  "cstmzs":[{"path":"./defs","name":"customization"}],
-  "headers":[{"path":"./defs","name":"header"}]
+  var confObjs={
+    "forms":[{"path":"./defs","formgroup":"","formname":"form", "formversion":""}],
+    "apps":[{"path":"./defs","name":"app"}],
+    "brands":[{"path":"./defs","name":"brand"}],
+    "cstmzs":[{"path":"./defs","name":"customization"}],
+    "headers":[{"path":"./defs","name":"header"}]
   }
