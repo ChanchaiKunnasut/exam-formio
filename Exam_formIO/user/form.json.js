e5aff48508757ebdb0a5f440fd808ad86f197806
ChanchaiKunnasut <c.kunnasut@togetherteam.co.th>
2018-03-07 15:28:11 +0700

    first commit

---
+++ Exam_formIO/user/form.json.js
@@ -0,0 +1,78 @@
+var formObj = 
+{
+    "type": "form",
+        "tags": [],
+            "access": [
+                {
+                    "roles": [
+                        "5a9e08b70867f336aae791a3"
+                    ],
+                    "type": "read_all"
+                }
+            ],
+                "submissionAccess": [
+                    {
+                        "roles": [
+                            "5a9e08b70867f336aae791a3"
+                        ],
+                        "type": "create_own"
+                    }
+                ],
+                    "owner": null,
+                        "components": [
+                            {
+                                "type": "email",
+                                "persistent": true,
+                                "unique": false,
+                                "protected": false,
+                                "defaultValue": "",
+                                "suffix": "",
+                                "prefix": "",
+                                "placeholder": "Enter your email address",
+                                "key": "email",
+                                "lockKey": true,
+                                "label": "Email",
+                                "inputType": "email",
+                                "tableView": true,
+                                "input": true
+                            },
+                            {
+                                "type": "password",
+                                "persistent": true,
+                                "protected": true,
+                                "suffix": "",
+                                "prefix": "",
+                                "placeholder": "Enter your password.",
+                                "key": "password",
+                                "lockKey": true,
+                                "label": "Password",
+                                "inputType": "password",
+                                "tableView": false,
+                                "input": true
+                            },
+                            {
+                                "type": "button",
+                                "theme": "primary",
+                                "disableOnInvalid": true,
+                                "action": "submit",
+                                "block": false,
+                                "rightIcon": "",
+                                "leftIcon": "",
+                                "size": "md",
+                                "key": "submit",
+                                "tableView": false,
+                                "label": "Submit",
+                                "input": true
+                            }
+                        ],
+                            "created": "2018-03-06T03:19:20.440Z",
+                                "revisions": "",
+                                    "_vid": 0,
+                                        "_id": "5a9e08b80867f392e6e791a6",
+                                            "title": "User Login",
+                                                "name": "userLogin",
+                                                    "path": "user/login",
+                                                        "machineName": "kxdizbgtfwhhkdm:userLogin",
+                                                            "project": "5a9e08b60867f34fa8e791a0",
+                                                                "modified": "2018-03-06T03:19:20.484Z"
+};
